253d9f8bb35cb3f02d2df8bc19f5a9a83b7141ad
cst204 <sutter1488@saskpolytech.ca>
2016-10-14 11:51:11 -0600

    did some stuff

---
+++ BysectionforPolynomials/BysectionforPolynomials/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BysectionforPolynomials
+{
+    public delegate double funcDecl(double dVal);
+    
+
+    class Program
+    {
+        /// <summary>
+        /// This will do the polynomial 3x^3-2x+2
+        /// </summary>
+        /// <param name="dVal"></param>
+        /// <returns></returns>
+        public static double equation1(double dVal)
+        {
+            return (double)(3 * Math.Pow(dVal, 3.0) - 2 * dVal + 2);
+        }
+
+        public static double equation2(double dVal)
+        {
+            return (double)(3 * Math.Pow(dVal, 4.0) - 2 * Math.Pow(dVal, 2.0) - 7 * dVal + 3);
+        }
+
+        public static double biSection(double dVal1, double dVal2, double dPrec, funcDecl myFunc)
+        {
+            double fLower = Math.Min(dVal1, dVal2);
+            double fUpper = Math.Max(dVal1, dVal2);
+            int nIteration = 0;
+
+            while (true)
+            {
+                double dMidPoint = (fLower + fUpper) / 2;
+                double dFofX = myFunc(dMidPoint);
+
+                Console.WriteLine("Lower{0:F6}\tUpper {1:F6}\tMidPoint {2:F6}\tF(x) {3:F6}",fLower,fUpper,dMidPoint,dFofX);
+
+                if(dFofX == 0 || Math.Abs(dFofX) < dPrec || nIteration++ > 50)
+                {
+                    return dMidPoint;
+                }
+
+                if(dFofX < 0)
+                {
+                    fLower = dMidPoint;
+                }
+                else
+                {
+                    fUpper = dMidPoint;
+
+                }
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The answer to the equation1 is " + biSection(-1.0, -2, 0.0001, equation1));
+
+            Console.WriteLine("The answer to the equation2 is " + biSection(0.0, 1.0, 0.0001, equation2));
+        }
+    }
+}
---
+++ babalonianMethod/babalonianMethod/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace babalonianMethod
+{
+    class Program
+    {
+        /// <summary>
+        /// This function will compute a square root for the value fVal
+        /// to a certain level of precision (fPrec). this method will use
+        /// the babylonian algorithm to aclomplish this.
+        /// </summary>
+        /// <param name="fVal"></param>
+        /// <param name="fPrec"></param>
+        /// <returns></returns>
+        static public float babylonian(float fVal, float fPrec)
+        {
+            float fX = 1.0f;
+
+            while(true)
+            {
+                float fXPrime = (float)0.5 * (fX + fVal / fX);
+                Console.WriteLine("fXPrime {0:F} and difference {1:F}", fXPrime, (fXPrime * fXPrime) - fVal);
+                if((fXPrime * fXPrime) - fVal <= fPrec)
+                {
+                    return fXPrime;
+                }
+                else
+                {
+                    fX = fXPrime;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Using Babylonian square, root of 16 is: " + babylonian(16.0f, 0.001f));
+        }
+    }
+}
